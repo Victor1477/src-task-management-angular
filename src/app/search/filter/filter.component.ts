b4b40d986a790ce06ff62dffde091310820e9f35
Joao Oliveira <Joao.Oliveira7@br.ey.com>
2024-02-23 12:39:17 -0300

    Create form

--- src/app/search/filter/filter.component.ts
+++ src/app/search/filter/filter.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+  inputForm: FormGroup = new FormGroup({
+    searchFilter: new FormControl('code'),
+  });
 
+  ngOnInit() {}
 }
